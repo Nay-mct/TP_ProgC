6a685c5d82dfc563827ed6b852f4f4b6daed6da6
Nay-mct <nayrrek.mct@gmail.com>
2026-09-18 09:27:37 +0000

    TP1: Exercice 1.10 - Pyramide de nombres

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+    int i;
+    int j;
+
+    for (i = 1; i <= n; i++) {
+        /* Espaces pour centrer la pyramide */
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        /* Partie croissante : de 1 à i */
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        /* Partie décroissante : de i - 1 à 1 */
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    printf("Génération de la pyramide terminée.\n");
+
+    return 0;
+}
 
