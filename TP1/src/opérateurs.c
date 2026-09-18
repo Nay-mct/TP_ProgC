84a3a7a51d713d2e7ebdcebb6d1ce646342c28e4
Nay-mct <nayrrek.mct@gmail.com>
2026-09-18 09:15:10 +0000

    TP1: Exercice 1.5 - Opérateurs arithmetiques et logiques

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int a = 16;
+    int b = 3;
+
+    /* Opérations arithmétiques */
+    printf("Addition (a + b)        : %d\n", a + b);
+    printf("Soustraction (a - b)     : %d\n", a - b);
+    printf("Multiplication (a * b)   : %d\n", a * b);
+    printf("Division entière (a / b) : %d\n", a / b);
+    printf("Modulo (a %% b)           : %d\n", a % b);
+
+    /* Opérateurs logiques / comparaisons (affichage valeur et libellé booléen) */
+    printf("a == b                   : %d (%s)\n", (a == b), (a == b) ? "true" : "false");
+    printf("a > b                    : %d (%s)\n", (a > b), (a > b) ? "true" : "false");
+
+    return 0;
+}
 
 
